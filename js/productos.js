aa7c1ecbbf8f907698413cc5ccaa4e7ef5ed0779
Iara Figueira <iarafigueiraa@gmail.com>
2022-08-06 18:02:03 +0200

    Avance2daEntrega

---
+++ js/productos.js
@@ -0,0 +1,74 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Alfacookie",
+    precio: 5340,
+    img: "../img/alfacookie.jpg",
+    cantidad: 1,
+    descripcion:
+      "Nuestro clásico alfacookie en versión XL! Dos discos de masa de cookie rellenos con dulce de leche. Peso aprox 1.8kg. Rinde para 12-15 personas.",
+  },
+  {
+    id: 2,
+    nombre: "Cheesecake Frutos Rojos",
+    precio: 5940,
+    img: "../img/cheesecake.jpg",
+    cantidad: 1,
+    descripcion:
+      "Cremoso cheesecake con base de galletitas de vainilla, decorado mermelada casera de frutos rojos. Tamaño 2kg para 12-18 personas aprox.",
+  },
+  {
+    id: 3,
+    nombre: "Chocotorta Oreo",
+    precio: 6600,
+    img: "../img/chocotortaoreo.jpg",
+    cantidad: 1,
+    descripcion:
+      "Clásica chocotorta con dulce de leche y queso crema rellena con Oreos y Chocolinas. Decorada con dulce de leche natural, migas de oreos y praliné.",
+  },
+  {
+    id: 4,
+    nombre: "Ferrero Nutella",
+    precio: 5340,
+    img: "../img/ferrero.jpg",
+    cantidad: 1,
+    descripcion:
+      "Torta con una base húmeda de chocolate, una deliciosa capa de Nutella, decorada con Ferrero Rocher y oblea crocante.",
+  },
+  {
+    id: 5,
+    nombre: "Chocotorta",
+    precio: 5940,
+    img: "../img/chocotorta.jpg",
+    cantidad: 1,
+    descripcion:
+      "Clásica chocotorta con dulce de leche y queso crema rellena con Chocolinas. Decorada con dulce de leche natural.",
+  },
+  {
+    id: 6,
+    nombre: "Marquise Beverly",
+    precio: 5940,
+    img: "../img/marquise.jpg",
+    cantidad: 1,
+    descripcion:
+      "Base húmeda de marquise sin harina, crema de avellanas, praliné de nueces, mousse de chocolate blanco y frutos rojos. 22cm para 12-15 personas.",
+  },
+  {
+    id: 7,
+    nombre: "Megabrownie",
+    precio: 5340,
+    img: "../img/megabrownie.jpg",
+    cantidad: 1,
+    descripcion:
+      "Base húmeda de Brownie, con dulce de leche y una crema de chocolate blanco, decorada con Marroc, chips de chocolate blanco y Kinder en barra.",
+  },
+  {
+    id: 8,
+    nombre: "Red Velvet",
+    precio: 6180,
+    img: "../img/redVelvet.jpg",
+    cantidad: 1,
+    descripcion:
+      "3 discos húmedos de vainilla con un leve sabor a cacao intercalados con frosting de vainilla y queso crema. Tamaño único rinde 12-15 porciones. 1.5kg aproximadamente.",
+  },
+];
--- js/scripts.js
+++ js/scripts.js
@@ -1,238 +1,71 @@
-let nombre = prompt("¿Cómo te llamas?");
-alert("Hola "+nombre+ "! Qué bueno verte por acá. \nA continuación te mostramos las tortas que disponibles.");
+const carritoUsuario = [];
 
-const productos = [
-  { id: 1, nombre: "Alfacookie", precio: 5340 },
-  { id: 2, nombre: "Cheesecake Frutos Rojos", precio: 5940 },
-  { id: 3, nombre: "Chocotorta Oreo", precio: 6600 },
-  { id: 4, nombre: "Ferrero Nutella", precio: 5340 },
-  { id: 5, nombre: "Chocotorta", precio: 5940 },
-  { id: 6, nombre: "Marquise Beverly", precio: 5940 },
-  { id: 7, nombre: "Megabrownie", precio: 5340 },
-  { id: 8, nombre: "Red Velvet", precio: 6180 },
-];
+const carrito = (IDdeProducto) => {
+  const compraUsuario = document.getElementById("compra-usuario");
+  const productoEnCarrito = () => {
+    let producto = productos.find(
+      (producto) => producto.id == IDdeProducto
+    );
+    carritoUsuario.push(producto);
+    producto.cantidad = 1;
+    let div = document.createElement("div");
+    div.classList.add(productoElegido);
+    div.innerHTML = `<p>${producto.nombre}</p>
+    <p>Precio: $${producto.precio}</p> 
+    <p id="cantidad${producto.id}">Cantidad: ${producto.cantidad}</p>
+    <button id="eliminar${producto.id}" class="eliminar" ><i class="fa-solid fa-trash-can"></i></button>`;
+    compraUsuario.appendChild(div);
+  };
+  productoEnCarrito();
+};
 
-let usuarioCompra = prompt(
-  "Ingresá el número de la torta que quieras llevar: \n1 - " +
-    productos[0].nombre +
-    " - $" +
-    productos[0].precio +
-    "\n2 - " +
-    productos[1].nombre +
-    " - $" +
-    productos[1].precio +
-    "\n3 - " +
-    productos[2].nombre +
-    " - $" +
-    productos[2].precio +
-    "\n4 - " +
-    productos[3].nombre +
-    " - $" +
-    productos[3].precio +
-    "\n5 - " +
-    productos[4].nombre +
-    " - $" +
-    productos[4].precio +
-    "\n6 - " +
-    productos[5].nombre +
-    " - $" +
-    productos[5].precio +
-    "\n7 - " +
-    productos[6].nombre +
-    " - $" +
-    productos[6].precio +
-    "\n8 - " +
-    productos[7].nombre +
-    " - $" +
-    productos[7].precio
-);
+const mostrarProductos = (productos) => {
+  const contenedorDeProductos = document.getElementById(
+    "contenedor-de-productos"
+  );
+  productos.forEach((producto) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `<div style="width: 18rem;">
+    <img src="${producto.img}" class="card-img-top" alt="${producto.nombre}">
+    <div class="card-body">
+      <h5 class="card-title text-center">${producto.nombre}</h5>
+      <p class="card-text">${producto.descripcion}</p>
+      <p class="card-text text-center">Precio: $${producto.precio}</p>
+      <button class="btn btn-primary" id=boton-producto${producto.id}>Añadir al carrito</button>
+    </div>
+  </div>`;
+    contenedorDeProductos.appendChild(div);
 
-const carrito = [];
+    const botonProducto = document.getElementById(
+      `boton-producto${producto.id}`
+    );
+    botonProducto.addEventListener("click", () => {
+      alert(`El producto ${producto.nombre} fue agregado al carrito`);
+      carrito(producto.id);
+    });
+  });
+};
 
-switch (usuarioCompra) {
-  // Alfacookie
-  case "1":
-    alert(
-      "Elegiste: " +
-        productos[0].nombre +
-        " - $" +
-        productos[0].precio +
-        ". \nContinua para elegir la forma de pago"
-    );
-    carrito.push((alfacookie = 5340));
-    break;
-  // Cheesecake
-  case "2":
-    alert(
-      "Elegiste: " +
-        productos[1].nombre +
-        " - $" +
-        productos[1].precio +
-        ". \nContinua para elegir la forma de pago"
-    );
-    carrito.push((cheesecake = 5940));
-    break;
-  // Chocotorta Oreo
-  case "3":
-    alert(
-      "Elegiste: " +
-        productos[2].nombre +
-        " - $" +
-        productos[2].precio +
-        ". \nContinua para elegir la forma de pago"
-    );
-    carrito.push((chocotortaOreo = 6600));
-    break;
-  // Ferrero Nutella
-  case "4":
-    alert(
-      "Elegiste: " +
-        productos[3].nombre +
-        " - $" +
-        productos[3].precio +
-        ". \nContinua para elegir la forma de pago"
-    );
-    carrito.push((ferreroNutella = 5340));
-    break;
-  // Chocotorta
-  case "5":
-    alert(
-      "Elegiste: " +
-        productos[4].nombre +
-        " - $" +
-        productos[4].precio +
-        ". \nContinua para elegir la forma de pago"
-    );
-    carrito.push((chocotorta = 5940));
-    break;
-  // Marquise Beverly
-  case "6":
-    alert(
-      "Elegiste: " +
-        productos[5].nombre +
-        " - $" +
-        productos[5].precio +
-        ". \nContinua para elegir la forma de pago"
-    );
-    carrito.push((marquise = 5940));
-    break;
-  // Megabrownie
-  case "7":
-    alert(
-      "Elegiste: " +
-        productos[6].nombre +
-        " - $" +
-        productos[6].precio +
-        ". \nContinua para elegir la forma de pago"
-    );
-    carrito.push((megabrownie = 5340));
-    break;
-  // Red Velvet
-  case "8":
-    alert(
-      "Elegiste: " +
-        productos[7].nombre +
-        " - $" +
-        productos[7].precio +
-        ". \nContinua para elegir la forma de pago"
-    );
-    carrito.push((redVelvet = 6180));
-    break;
-}
+mostrarProductos(productos);
 
-//simulación de pago
-let pago = prompt(
-  "Seleccione la forma de pago \n1- Efectivo (15% de descuento) \n2- Tarjeta de débito \n3- Tarjeta de crédito \n4 - Transferencia (5% de descuento)"
-);
+const modal = document.querySelector(".contenedor-modal");
+const abrirCarrito = document.getElementById("abrir");
+const cerrarCarrito = document.getElementById("cerrar)");
+const modalCarrito = document.queryselector(".carrito-modal");
 
-// while (cuotas != 1 || cuotas != 2 || cuotas != 3 || cuotas != 4) { alert("La opción elegida no es correcta");
+abrirCarrito.addEventListener("click", () => {
+  modal.classList.toggle("modal-visible");
+});
 
-switch (pago) {
-  //Efectivo
-  case "1":
-    let porcentaje = 15;
-    function calcularPorcentaje(porcentaje, carrito) {
-      return (porcentaje * carrito) / 100;
-    }
-    alert(
-      "El descuento por abonar en efectivo es $" +
-        calcularPorcentaje(porcentaje, carrito) +
-        ". Deberás abonar $" +
-        (carrito - calcularPorcentaje(porcentaje, carrito))
-    );
-    break;
+cerrarCarrito.addEventListener("click", () => {
+  modal.classList.remove("modal-visible");
+});
 
-  //Tarjeta de débito
-  case "2":
-    alert("El monto a abonar es $" + carrito);
-    break;
+modal.addEventListener("click", () => {
+  cerrarCarrito.click();
+});
 
-  //Tarjeta de crédito
-  case "3":
-    let credito = prompt(
-      "Seleccione las cuotas en las que desea abonar \n1- 1 cuota. \n2- 3 cuotas. \n3- 6 cuotas. \n4 - 12 cuotas."
-    );
-    switch (credito) {
-      // 1 cuota
-      case "1":
-        alert("El monto a pagar es un total de $" + carrito);
-        break;
-      // 3 cuotas
-      case "2":
-        let porcentajeTresCuotas = 15;
-        function tresCuotas(porcentajeTresCuotas, carrito) {
-          return (porcentajeTresCuotas * carrito) / 100;
-        }
-        alert(
-          "El recargo por abonar en tres cuotas es $" +
-            tresCuotas(porcentajeTresCuotas, carrito) +
-            ". El monto a pagar es un total de $" +
-            (carrito - tresCuotas(porcentajeTresCuotas, carrito))
-        );
-        break;
-      // 6 cuotas
-      case "3":
-        let porcentajeSeisCuotas = 25;
-        function seisCuotas(porcentajeSeisCuotas, carrito) {
-          return (porcentajeSeisCuotas * carrito) / 100;
-        }
-        alert(
-          "El recargo por abonar en seis cuotas es $" +
-            seisCuotas(porcentajeSeisCuotas, carrito) +
-            ". El monto a pagar es un total de $" +
-            (carrito - seisCuotas(porcentajeSeisCuotas, carrito))
-        );
-        break;
-      // 12 cuotas
-      case "4":
-        let porcentajeDoceCuotas = 35;
-        function doceCuotas(porcentajeDoceCuotas, carrito) {
-          return (porcentajeDoceCuotas * carrito) / 100;
-        }
-        alert(
-          "El recargo por abonar en doce cuotas es $" +
-            doceCuotas(porcentajeDoceCuotas, carrito) +
-            ". El monto a pagar es un total de $" +
-            (carrito - doceCuotas(porcentajeDoceCuotas, carrito))
-        );
-        break;
-    }
-    break;
-  //Transferencia
-  case "4":
-    let descuentoTransferencia = 5;
-    function calcularTransferencia(descuentoTransferencia, carrito) {
-      return (descuentoTransferencia * carrito) / 100;
-    }
-    alert(
-      "El descuento por realizar el pago mediante transferencia es $" +
-        calcularTransferencia(descuentoTransferencia, carrito) +
-        ". Deberás abonar $" +
-        (carrito - calcularTransferencia(descuentoTransferencia, carrito))
-    );
-    break;
-}
-// pago = prompt("Seleccione la forma de pago \n1- Efectivo (15% de descuento) \n2- Tarjeta de crédito \n3- Tarjeta de débito \n4 - Transferencia"); };
-
-let gracias = alert("Muchas gracias por tu compra, esperamos que la disfrutes y que vuelvas pronto por aca! :)");
-
+modalCarrito.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
